e8215839c20182e87fe8a31648e5e78dd3803577
NathanStein1 <nathanstein2@gmail.com>
2022-04-28 12:36:54 -0700

    JS

--- script.js
+++ script.js
@@ -1,50 +1,56 @@
-var query = document.querySelector('.queryInput').value
-var apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=` + query + `&appid=302b19a6a4300fb8db031ad4aeaefe4c`
+// var query = document.querySelector('.queryInput').value
+// var apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=` + query + `&appid=302b19a6a4300fb8db031ad4aeaefe4c`
 
 
 
-function getApi(apiURL) {
-    fetch(apiURL)
-        .then(function (response) {
-            console.log(response);
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-        
-
-}
-getApi();
-
-function setformatParam(e) {
-    
-    e.preventDefault()
+// function getApi(apiURL) {
+//     fetch(apiURL)
+//         .then(function (response) {
+//             console.log(response);
+//             return response.json()
+//         })
+//         .then(function (data) {
+// //             console.log(data);
+// test(data)
+// //         });
 
 
-    var formatParam = ""
-    var query = document.querySelector('.queryInput').value
-    
-    var apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=` + query + `&appid=302b19a6a4300fb8db031ad4aeaefe4c`
+// // }
+
+// function test(data){
+
+// console.log(data)
+// }
+// getApi();
+
+// function setformatParam(e) {
+
+//     e.preventDefault()
 
 
-    getApi(apiURL);
+//     var formatParam = ""
+//     var query = document.querySelector('.queryInput').value
 
-    var weatherBox = document.querySelector('.weatherSection')
-
-
-    weatherBox.textContent = "Need to call data.list[0].clouds.value"
+//     var apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=` + query + `&appid=302b19a6a4300fb8db031ad4aeaefe4c`
 
 
-//   For loop 
+//     getApi(apiURL);
+
+//     var weatherBox = document.querySelector('.weatherSection')
+
+
+//     weatherBox.textContent = "Need to call data.list[0].clouds.value"
+
+
+// //   For loop 
 
 
 
-}
+// }
 
-var submitBut = document.querySelector('.citysearchBtn')
+// var submitBut = document.querySelector('.citysearchBtn')
 
-submitBut.addEventListener('click', setformatParam)
+// submitBut.addEventListener('click', setformatParam)
 
 
 
@@ -56,29 +62,49 @@ submitBut.addEventListener('click', setformatParam)
 
 
 
-// var weatherBox = document.querySelector('.weatherSection')
-// var query = document.querySelector('.queryInput').value
+var weatherBox = document.querySelector('.weatherSection')
 
 
 
-// const getData = async () => {
-//     const res = await fetch(
-//         `http://api.openweathermap.org/data/2.5/forecast?q=` + query + `&appid=302b19a6a4300fb8db031ad4aeaefe4c`,
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             Accept: "application/json; odata=verbose",
-//           },
-//         }
-//       )
-//     const data = await res.json();
-//     const result = data.list[0].clouds.value;
-//     console.log(result)
-//     return result;
-// }
 
-// getData().then((res) => {
-//     weatherBox.textContent = res;
-// })
+const getData = async () => {
+    var query = document.querySelector('.queryInput').value
+    try {
+        const res = await fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=` + query + `&appid=302b19a6a4300fb8db031ad4aeaefe4c&units=imperial`
 
-// getData();
+        )
+        const data = await res.json();
+        console.log(data)
+        var i = 0;
+        for (i = 0; i < 5; i++) {
+            var weatherChild = document.createElement('div')
+            weatherChild.classList.add('weatherChild')
+            weatherChild.textContent = data.list[i].weather[0].description
+
+            var weatherChildTemp = document.createElement('div')
+            weatherChildTemp.textContent = data.list[i].main.temp
+            weatherChild.appendChild(weatherChildTemp)
+
+            weatherBox.appendChild(weatherChild);
+            console.log(data.list[i])
+
+        }
+
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+
+var submitBut = document.querySelector('.citysearchBtn')
+
+submitBut.addEventListener('click', getData)
+
+
+// loop it generate divs i<5 , grab icons, temp, day
